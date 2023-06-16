bd351f8436a17e037602faf3680e97fa84278369
Serhat KAYA <serhatkaya.sr@gmail.com>
2023-06-17 01:14:22 +0300

    add sidebar outerclick close feature

--- projects/velocity-ui/src/components/sidebar/sidebar.component.ts
+++ projects/velocity-ui/src/components/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Observable, of, tap } from 'rxjs';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Observable, delay, of, tap } from 'rxjs';
 import { VelocitySidebarService } from './sidebar.service';
 
 @Component({
@@ -8,6 +8,7 @@ import { VelocitySidebarService } from './sidebar.service';
 })
 export class VelocitySidebarComponent implements OnInit {
   constructor(protected sidebarService: VelocitySidebarService) {}
+  @ViewChild('wrapper', { static: true }) wrapper!: ElementRef;
 
   @Input() id!: string;
   @Input() title!: string;
@@ -15,14 +16,23 @@ export class VelocitySidebarComponent implements OnInit {
   @Input() position: 'left' | 'right' = 'right';
   public isOpen = false;
   public isOpen$: Observable<boolean> = of(false);
+  public isOpenDelayed$: Observable<boolean> = of(false);
 
   ngOnInit(): void {
     this.isOpen$ = this.sidebarService
       .isOpenAsync(this.id)
       .pipe(tap((state: boolean) => (this.isOpen = state)));
+
+    this.isOpenDelayed$ = this.isOpen$.pipe(delay(100));
   }
 
   public closeSidebar(): void {
     this.sidebarService.close(this.id);
+  }
+
+  onOuterClick(target: HTMLElement) {
+    if (!this.wrapper.nativeElement.contains(target)) {
+      this.closeSidebar();
+    }
   }
 }
--- projects/velocity-ui/src/components/sidebar/sidebar.module.ts
+++ projects/velocity-ui/src/components/sidebar/sidebar.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { VelocityDirectivesModule } from '../../directives/directives.module';
 import { VelocitySidebarComponent } from './sidebar.component';
 
 @NgModule({
   declarations: [VelocitySidebarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, VelocityDirectivesModule],
   exports: [VelocitySidebarComponent],
 })
 export class VelocitySidebarModule {}
