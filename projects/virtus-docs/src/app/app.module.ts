34de8db5498c9827a975dcd3f6f239caf132b9d0
Serhat KAYA <serhatkaya.sr@gmail.com>
2023-07-27 03:04:28 +0300

    virtus docs

--- projects/virtus-docs/src/app/app.module.ts
+++ projects/virtus-docs/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {
+  VirtusAccordionModule,
+  VirtusDropdownModule,
+  VirtusResponsiveHeaderModule,
+} from 'virtus-ui';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    VirtusResponsiveHeaderModule,
+    VirtusAccordionModule,
+    VirtusDropdownModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent,
+      },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
